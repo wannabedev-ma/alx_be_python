b9b4b342de820d63a1d1b2d0e05d6753b4d482c3
wannabedev-ma <dev@192.168.1.238>
2024-09-27 12:23:17 +0100

    shopping_list_manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,10 +12,10 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-		item = input('enter an item')
+		item = input('Enter the item to add: ')
 		shopping_list.append(item)
         elif choice == '2':
-        	item = input('enter an item')
+        	item = input('Enter the item to remove: ')
 		shopping_list.remove(item)
         elif choice == '3':
 		for item in shopping_list:
