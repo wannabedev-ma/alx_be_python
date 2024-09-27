b03108dea73ac099c26c266391b55bf0416948f6
wannabedev-ma <dev@192.168.1.238>
2024-09-27 04:33:19 +0100

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
 		case 'multiply':
 			return(num1*num2)
 		case 'divide':
-			if num2 != 0:
+			if num2 == 0:
 				return(num1/num2)
 			else:i
 				return('not divisible by 0')
