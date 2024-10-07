96ac4cefc923eab23e910716f8f62661b62a9450
wannabedev-ma <63165738+wannabedev-ma@users.noreply.github.com>
2024-10-07 04:27:29 +0100

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,20 @@
 class BankAccount:
+    def __init__(self, initial_balance=0): #initialize an account_balance attribute with optional starting balalnce defaulting to zero
+        self.account_balance = initial_balance
 
-	def __init__(self,account_balance):
-		self.account_balance = 0
+    def deposit(self, amount):
+        if amount > 0:
+            self.account_balance += amount
+            return "Deposited"
+        else:
+            print("Deposit amount must be above zero")
 
-	def deposit(self, amount):
-        	if amount > 0:
-            		self.account_balance += amount
-            		return "Deposited"
-        	else:
-           		print("Deposit amount must be above zero")
-		
-	def withdraw(self, amount):
-        	if 0 < amount <= self.account_balance :
-            		self.account_balance -= amount
-            		return True  
-        	else:
-            		return False
-
-	def display_balance(self):
-		print(f"Current Balance: ${self.account_balance:.2f}") 
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+        
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance:.2f}") 
