98a09861b8c583bcd72ccb68eb51f22a06a9722a
wannabedev-ma <63165738+wannabedev-ma@users.noreply.github.com>
2024-10-07 04:13:43 +0100

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,25 +2,28 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 def TestSimpleCalculator(unitest.testCase):
+
+	def setUp(self):
+        	self.calc = SimpleCalculator()
 	
 	test_add(self):
-		self.assertEqual(add(3,4),7)
-		self.assertEqual(add(3,-4),-1)
+		self.assertEqual(calc.add(3,4),7)
+		self.assertEqual(calc.add(3,-4),-1)
 
 	test_subtract(self):
-		self.assertEqual(subtract(4,3),1)
-		self.assertEqual(subtract(4,-3),7)
+		self.assertEqual(calc.subtract(4,3),1)
+		self.assertEqual(calc.subtract(4,-3),7)
 
 	test_multiply(self):
-		self.assertEqual(multiply(3,4),12)
-		self.assertEqual(multiply(3,0),0)
-		self.assertEqual(multiply(3,-4),-12)
+		self.assertEqual(calc.multiply(3,4),12)
+		self.assertEqual(calc.multiply(3,0),0)
+		self.assertEqual(calc.multiply(3,-4),-12)
 
 	test_divide(self):
-		self.assertEqual(devide(4,2),2)
-		self.assertEqual(devide(3,2),1.5)
+		self.assertEqual(calc.devide(4,2),2)
+		self.assertEqual(calc.devide(3,2),1.5)
 		with self.assertRaises(ZeroDivisionError) :
-			divide(4,0)
+			calc.divide(4,0)
 		
 if __name__ == '__main__':
     unittest.main()
